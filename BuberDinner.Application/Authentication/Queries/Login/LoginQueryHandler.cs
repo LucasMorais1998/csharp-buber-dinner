231cb7101c0a2b22dc4500a52fb64796a3f6ef3d
LucasMorais1998 <lucas14.morais@gmail.com>
2023-11-08 23:57:37 -0300

    feat: add LoginQueryHandler

---
+++ BuberDinner.Application/Authentication/Queries/Login/LoginQueryHandler.cs
@@ -0,0 +1,42 @@
+using BuberDinner.Application.Authentication.Common;
+using BuberDinner.Application.Authentication.Queries.Login;
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Application.Common.Interfaces.Persistence;
+using BuberDinner.Domain.Common.Errors;
+using BuberDinner.Domain.Entities;
+using ErrorOr;
+using MediatR;
+
+namespace BuberDinner.Application.Authentication.Commands.Login;
+
+public class LoginCommandHandler :
+    IRequestHandler<LoginQuery, ErrorOr<AuthenticationResult>>
+{
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
+
+    public LoginCommandHandler(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+        _userRepository = userRepository;
+    }
+    public async Task<ErrorOr<AuthenticationResult>> Handle(LoginQuery query, CancellationToken ct)
+    {
+
+       if (_userRepository.GetUserByEmail(query.Email) is not User user)
+        {
+            return Errors.Authentication.InvalidCredentials;
+        }
+
+        if (user.Password != query.Password)
+        {
+            return new[] { Errors.Authentication.InvalidCredentials };
+        }
+
+        var token = _jwtTokenGenerator.GenerateToken(user);
+
+        return new AuthenticationResult(
+            user,
+            token);
+    }
+}
